72e6bc23a442480d163f31880b134db916e5183d
Aditya <yraditya895@gmail.com>
2025-03-26 22:09:13 +0530

    transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -21,6 +21,56 @@ class DataTransformation:
     
     def get_data_transformer_object(self):
         try:
-            pass
+            numerical_features = ['writing_score', 'reading_score']
+            categorical_features = [
+                'gender',
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='median')),
+                    ('scaler', StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps = [
+                    ('imputer', SimpleImputer(strategy='mst_frequent', fill_value='missing')),
+                     ('one_hot_encoder', OneHotEncoder(handle_unknown='ignore')),
+                     ('scaler', StandardScaler())
+                ]
+            )
+            logging.info("numerical features scaling (standard) completed")
+            logging.info("Categorical encoding completed")
+
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline, numerical_features)
+                ('cat_pipeline', cat_pipeline, categorical_features)
+                ]    
+            )
+
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("The train and test dataframes are created")
+            logging.info("Obtaining preprocessing object")
+
+            preprocessor_obj = self.get_data_transformer_object()
+            target_column_name = "math_score"
+            numerical_columns = ['writing_score', 'reading_score']
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
         except:
             pass
